329b6363a6d70480e365f26972f5991abe825d03
xomarnd <smirnov.inv@gmail.com>
2019-12-10 14:34:53 +0300

    Create project!

---
+++ desktop/src/ru/geekbrains/mystargame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.mystargame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geekbrains.mystargame.MyStarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyStarGame(), config);
+	}
+}
